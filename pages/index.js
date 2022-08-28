3b6ce21424586db31f2158a43d2060fd220e374f
Blake Frederick <bf@six12creative.com>
2022-08-28 10:27:32 -0700

    remove link to broken book search

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
     <div className="flex flex-col md:px-12 px-0 relative bg-background font-raleway items-center min-h-screen">
       <h1 className="text-6xl font-bold text-active mt-20">Typical Recipe Search</h1>
       <h2 className="text-primary text-2xl font-light mt-5">
-        Just a typical recipe search. That's it. <Link href="/books"><a className="underline">(and books)</a></Link>
+        Just a typical recipe search. That's it. {/* <Link href="/books"><a className="underline">(and books)</a></Link> */}
       </h2>
         <p className="block text-primary text-sm">{error}</p>
       <form
