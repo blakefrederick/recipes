35224362df5cfbde70ba03a62fbf8bd9e129f0da
Blake Frederick <bf@six12creative.com>
2022-08-28 22:41:14 -0700

    Count properly

--- pages/api/credits.js
+++ pages/api/credits.js
@@ -3,14 +3,13 @@ import db from '../../lib/postgres'
 export default async function handler(req, res) {
 
   const endpoint = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-  const query = `SELECT COUNT(*) from blakefrederick WHERE DATE(time) = date(timezone('PST', now())) AND endpoint = $1`
-  const creditsPerDay = 40;
+  const query = `SELECT COUNT(*) from blakefrederick WHERE DATE(time) = DATE(NOW()) AND endpoint = $1`
+  const creditsPerDay = 42;
   const params = [endpoint]
 
   try {
     // pool.query() will escape parameterized query
     db.query(query, params, (error, result) => {
-      console.log('result', result?.rows[0]?.count)
       const creditsRemaining = result?.rows[0]?.count !==  undefined ? creditsPerDay - result.rows[0].count : 0 
       return res.status(201).send({creditsRemaining})
     })
